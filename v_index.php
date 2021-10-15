500662ed748859340e81e28f6d0e71f4dd07d150
WP-Yudiiskandar <yudiiskandar1999@gmail.com>
2021-10-15 19:54:29 +0700

    membuat template web sederhana

---
+++ v_index.php
@@ -0,0 +1,24 @@
+			<section>
+				<h1><?php echo $judul ?></h1>
+				<p align="justify">Pada pengertian codeigniter di atas tadi di jelaskan bahwa codeigniter menggunakan metode
+			MVC. Apa itu MVC? kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih jauh dalam
+			belajar codeigniter.</p>
+			<p>MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi tiga komponen yaitu 
+			model, view dan controller.</p>
+				<ol type="A"></ol>
+				<li>Model</li>
+<p align="justify">Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan digunakan
+oleh aplikasi.</p>
+<p align="justify">Sebagai catatan, Semua model harus disimpan di dalam folder application\models</p>
+
+<li>View</li>
+<p align="justify">View merupakan bagian yang menangani halaman user interface atau halaman yang muncul 
+pada user(pada browser).</p>
+<p align="justify">tampilan dari user interface di kumpulkan pada folde 
+Application/views</p>
+<li>Controller</li>
+<p align="justify">Controller merupakan kumpulan intruksi aksi yang menghubungkan model dan view
+Untuk bentuk umum dan contoh-contoh penggunaan model, view, controller serta hasilnya bisa
+dilihat pada modul halaman 9.</p>
+
+			</section>
